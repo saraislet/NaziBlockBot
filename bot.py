77f97b8640aa291857997e8afac4d7729fc3e55c
Sarai <anna.sarai.rosenberg@gmail.com>
2017-10-06 03:39:25 -0700

    clean tweet contents for insertion

--- bot.py
+++ bot.py
@@ -5,8 +5,7 @@ Created on Thu Oct  5 16:50:36 2017
 @author: Sarai
 """
 
-import sys
-import json
+import json, sys, re, requests
 import tweepy
 from tweepy.streaming import StreamListener
 import pymysql.cursors
@@ -68,15 +67,25 @@ def insert_receipt(dm):
     # Insert DM contents into DB receipts table
     sender_id = dm['sender_id']
     recipient_id = dm['recipient_id']
-    text = dm['text']
+    contents = dm['text']
+    parsed_text = parse_url_from_text(contents)
+    text = parsed_text[0]
+    tweet_url = parsed_text[1]
+    if verify_twitter_url(tweet_url):
+        status = get_tweet_from_url(tweet_url)
+        twitter_id = status.user.id
+        screen_name = status.user.screen_name
+        name = status.user.name
+        tweet = unshorten_urls_in_text(status.full_text)
+        tweet_text = remove_ats(tweet)
     
     connection = db_connect()
     
     try:
         with connection.cursor() as cursor:
             # Create a new record in receipts table
-            sql = "INSERT INTO `receipts` (`twitter_id`, `blocklist_id`, `contents_text`) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (sender_id, recipient_id, text))
+            sql = "INSERT INTO `receipts` (`twitter_id`, `name`, `screen_name`, `blocklist_id`, `contents_text`, `url`, `source_user_id`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (twitter_id, name, screen_name, recipient_id, tweet_text, tweet_url, sender_id))
     
         # Commit to save changes
         connection.commit()
@@ -86,13 +95,73 @@ def insert_receipt(dm):
             sql = "SELECT `id` FROM `receipts` WHERE `twitter_id`=%s AND `blocklist_id`=%s LIMIT 1"
             cursor.execute(sql, (sender_id,recipient_id,))
             result = cursor.fetchone()
+            return "Successfully inserted DM into receipts database, id " + str(result)
                 
     except BaseException as e:
         return "Error in insert_receipt()"
 
     finally:
         connection.close()
-        return "Successfully inserted DM into receipts database, id " + str(result)
+
+
+def verify_twitter_url(url):
+    # Verify that a URL belongs to a Twitter status
+    match = re.match(r'https://twitter\.com/.+/status/\d+', url)
+    
+    if match != None:
+        return True
+    else:
+        return False
+
+
+def get_tweet_from_url(url):
+    # Pull twitter status details from API and return dictionary.
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.secure = True
+    auth.set_access_token(config.access_token, config.access_token_secret)
+    api = tweepy.API(auth)
+    
+    status_id = re.sub(r'https://twitter\.com/.+/status/(\d+)', r'\1', url)
+    
+    # Extended tweet mode returns full text without truncation.
+    return api.get_status(status_id, tweet_mode='extended')    
+
+
+def parse_url_from_text(string):
+    # Return an array with text and url from the string of a DM.
+    
+    # Match any URL beginning "http" at the end of string text.
+    match = re.match(r'(.*)\s(http\S*)$', string)
+
+    if match != None:
+        text = match.group(1)
+        url = match.group(2)
+        return [text, unshorten_url(url)]
+    else:
+        return [string, ""]
+
+
+def unshorten_url(url):
+    # Return expanded URL (or same URL if not a redirect)
+    return requests.head(url, allow_redirects=True).url
+
+
+def unshorten_url_re(url):
+    # Return expanded URL from regex match.
+    # Unclear how to combine this with previous function nontrivially.
+    # This works for now.
+    return requests.head(url.group(), allow_redirects=True).url
+
+
+def unshorten_urls_in_text(string):
+    # Unshorten all URLs in a string.
+    return re.sub(r'(https?://\S*)', unshorten_url_re, string)
+
+
+def remove_ats(tweet):
+    # Remove any leading @s (e.g., replies) from a tweet.
+    # Any @ that is not at the beginning of a tweet will be left.
+    return re.sub(r'^(@\S+\s)*', "", tweet)
 
 
 def main():
